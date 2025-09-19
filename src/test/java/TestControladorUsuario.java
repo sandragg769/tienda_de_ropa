7ee88dc427bc36515af6696669044e25c200a770
Sandra <sandragg67@gmail.com>
2025-09-19 11:36:40 +0200

    pom y test

---
+++ src/test/java/TestControladorUsuario.java
@@ -0,0 +1,11 @@
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestControladorUsuario {
+    @BeforeEach
+    void setUp() {
+
+    }
+}
