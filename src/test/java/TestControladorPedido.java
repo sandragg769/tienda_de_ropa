f3796053dd1f486cf777965e8e5c73d3bda5314f
Sandra Gómez García <sandragm67@gmail.com>
2025-09-23 19:33:57 +0200

    model, controlador usu y pedido y test usu

---
+++ src/test/java/TestControladorPedido.java
@@ -0,0 +1,19 @@
+
+import org.example.controller.ControladorUsuario;
+import org.example.model.Usuario;
+import org.example.model.producto.Etiqueta;
+import org.example.model.producto.Producto;
+import org.example.model.producto.enumeraciones.Color;
+import org.example.model.producto.enumeraciones.Talla;
+import org.example.model.producto.tipoDeProductos.Camisa;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDate;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TestControladorPedido {
+
+
+}
