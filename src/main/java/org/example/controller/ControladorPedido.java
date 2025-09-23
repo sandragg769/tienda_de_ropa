5d99981e4b91440d2e482f04d142400d3c859c9b
Sandra <sandragg67@gmail.com>
2025-09-23 09:23:19 +0200

    controlador usu y test hecho, controlador pedido empezado

--- src/main/java/org/example/controller/ControladorPedido.java
+++ src/main/java/org/example/controller/ControladorPedido.java
@@ -1,5 +1,17 @@
 package org.example.controller;
 
+import org.example.model.pedido.Pedido;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class ControladorPedido {
+    private List<Pedido> listaPedidos=new ArrayList<>();
+    private long contadorPedidos=0;
+    //el contador de lineaPedido también se controla aquí
+    private  long contadorLineaPedido=0;
+
+    //tenemos que tener el controlador para acceder a los usuarios para usar sus métodos
+    private ControladorUsuario controladorUsuario;
 
 }
